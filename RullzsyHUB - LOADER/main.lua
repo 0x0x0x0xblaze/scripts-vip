bc798f88d6c38f5eb3137f668484aada95ae1dc9
0xtunggereung@gmail.com <rullzsyhub@gmail.com>
2025-10-27 16:22:04 +0700

    first commit

---
+++ RullzsyHUB - LOADER/main.lua
@@ -0,0 +1,25 @@
+-- Load Liblary UI
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+-- Window Process
+local Window = Rayfield:CreateWindow({
+	Icon = "braces",
+    Name = "RullzsyHUB | Script Loader | VIP-KING | 14 DAYS",
+    LoadingTitle = "Created By RullzsyHUB",
+    LoadingSubtitle = "Follow Tiktok: @rullzsy99",
+})
+
+-- Tab Menu
+local ScriptTab = Window:CreateTab("List Scripts", 4483362458)
+
+-- Section
+ScriptTab:CreateSection("🟢 TOTAL MAP: 1")
+
+ScriptTab:CreateButton({
+    Name = "🟢 MOUNT YAHAYUK",
+    Callback = function()
+        Rayfield:Notify({Title="Executing", Image="file", Content="Loading MOUNT YAHAYUK...", Duration=4})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/0x0x0x0xblaze/scripts-vip/refs/heads/main/RullzsyHUB%20-%20MOUNT%20YAHAYUK/main.lua"))()
+    end
+})
+
