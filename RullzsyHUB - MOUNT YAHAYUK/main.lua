4a08cf1c57a72cdcd5b611c1cd850b8552ff4f0e
0xtunggereung@gmail.com <rullzsyhub@gmail.com>
2025-10-27 16:35:14 +0700

    first commit

--- RullzsyHUB - MOUNT YAHAYUK/main.lua
+++ RullzsyHUB - MOUNT YAHAYUK/main.lua
@@ -144,7 +144,7 @@ BypassTab:CreateToggle({
 -----| AUTO WALK VARIABLES |-----
 -- Setup folder save file json
 local mainFolder = "RullzsyHUB"
-local jsonFolder = mainFolder .. "/js_mount_yahayuk_vip_patch_new_003"
+local jsonFolder = mainFolder .. "/json_vip_yahayuk_patch_001"
 if not isfolder(mainFolder) then
     makefolder(mainFolder)
 end
@@ -153,7 +153,7 @@ if not isfolder(jsonFolder) then
 end
 
 -- Server URL and JSON checkpoint file list
-local baseURL = "https://raw.githubusercontent.com/0x0x0x0xblaze/json/refs/heads/main/json_mount_yahayuk_vip/"
+local baseURL = "https://raw.githubusercontent.com/0x0x0x0xblaze/json-vip/refs/heads/main/json_mount_yahayuk_vip/"
 local jsonFiles = {
     "spawnpoint.json",
     "checkpoint_1.json",
