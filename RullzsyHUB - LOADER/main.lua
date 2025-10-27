f5a06e071f457c7e0422fef48d8ab4c689e7955f
0xtunggereung@gmail.com <rullzsyhub@gmail.com>
2025-10-27 21:54:08 +0700

    first commit

--- RullzsyHUB - LOADER/main.lua
+++ RullzsyHUB - LOADER/main.lua
@@ -56,6 +56,14 @@ ScriptTab:CreateButton({
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/0x0x0x0xblaze/scripts-vip/refs/heads/main/RullzsyHUB%20-%20MOUNT%20ANJ/main.lua"))()
 	end
 })
+
+ScriptTab:CreateButton({
+	Name = "🟢 MOUNT YNTKTS | VIP-KING | 14 DAYS",
+	Callback = function()
+		Rayfield:Notify({Title="Executing", Image="file", Content="Loading MOUNT YNTKTS...", Duration=4})
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/0x0x0x0xblaze/scripts-vip/refs/heads/main/RullzsyHUB%20-%20MOUNT%20YNTKTS/main.lua"))()
+	end
+})
 -- =============================================================
 -- UPDATE SCRIPT
 -- =============================================================
